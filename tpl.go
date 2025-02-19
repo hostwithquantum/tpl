2199230fcc73b65956e66f2586e3e7dfd9745c94
Till Klampaeckel <till@php.net>
2025-02-19 13:48:00 +0100

    Chore(docs): godoc

--- tpl.go
+++ tpl.go
@@ -1,3 +1,4 @@
+// Package tpl wraps around html/template to make dealing with templates less cumbersome.
 package tpl
 
 import (
@@ -9,15 +10,18 @@ import (
 	"net/http"
 )
 
+// Render represents the helper and has a Render function.
 type Render struct {
 	fs   embed.FS
 	name string
 }
 
+// NewRender creates Render struct.
 func NewRender(fs embed.FS, name string) *Render {
 	return &Render{fs, name}
 }
 
+// Render executes the template and flushes it via the http.ResponseWriter.
 func (r *Render) Render(w http.ResponseWriter, tmpl string, data any) {
 	t := template.New(r.name).Funcs(sprig.FuncMap())
 
